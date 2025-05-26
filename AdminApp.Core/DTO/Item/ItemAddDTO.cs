78ca2eb84f7998a6812fcffc0fc8eac73a11c604
MIGUEL ENRIQUE GAMBOA ROMERO <miguel.gamboa@sistemasentry.com.co>
2025-05-25 21:58:14 -0500

    Se añaden las propiedades de descripción e imagen a la entidad de item

--- AdminApp.Core/DTO/Item/ItemAddDTO.cs
+++ AdminApp.Core/DTO/Item/ItemAddDTO.cs
@@ -3,6 +3,8 @@
     public class ItemAddDTO
     {
         public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; }
+        public string Image { get; set; } = string.Empty;
     }
 }
--- AdminApp.Core/Entities/Item.cs
+++ AdminApp.Core/Entities/Item.cs
@@ -4,6 +4,8 @@
     {
         public int ItemId { get; set; }
         public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; }
+        public string Image { get; set; } = string.Empty;
     }
 }
