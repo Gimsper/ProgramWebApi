94081f0132e761795d3430cfc33dbd90b9a1d034
MIGUEL ENRIQUE GAMBOA ROMERO <miguel.gamboa@sistemasentry.com.co>
2025-05-26 22:51:18 -0500

    Se añade migración

---
+++ AdminApp.Core/Migrations/20250527034952_AddDescriptionAndImage.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AdminApp.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescriptionAndImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Item",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Item",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Item");
+
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Item");
+        }
+    }
+}
